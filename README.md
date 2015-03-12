233cdd2e05dd1e27e3e9dab2f450fff432694250
Taureli <hobkinss@gmail.com>
2015-03-12 14:24:43 +0100

    Detecting modifiers

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Autor: Jakub Karolczak
 ##Krótki opis programu:
 [Główny kod programu](https://github.com/Taureli/JSON-converter/blob/master/src/Converter.java).
 
-Prosty program konwertujący klasy Javy na obiekty JSON. Konwerter obsługuje typy proste (rozróżniając typy znakowe i liczbowe) oraz tablice wielowymiarowe typów prostych. Program wykrywa jedynie pola publiczne.
+Prosty program konwertujący klasy Javy na obiekty JSON. Konwerter obsługuje typy proste (rozróżniając typy znakowe i liczbowe) oraz tablice wielowymiarowe typów prostych. Program wykrywa wszystkie pola, niezależnie czy są publiczne czy prywatne.
 
 #####Przykład wywołania konwertera:
 ```Java
--- src/Converter.java
+++ src/Converter.java
@@ -1,5 +1,6 @@
 import java.lang.reflect.Array;
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 
 class Person {
 	public String name = "Jakub";
@@ -9,6 +10,8 @@ class Person {
 	public float testfloat = 12.34f;
 	public int[] tab = {2, 3, 4};
 	public int[][] t = new int[3][3];
+	private String prywatne = "pole";
+	static int statyczne = 1234;
 	
 	public Person(){
 		t[0][1] = 10;
@@ -39,23 +42,32 @@ class Converter {
 		
 	}
 	
+	public static void convert(Field f, Object a) throws IllegalArgumentException, IllegalAccessException{
+		//Konwersja dla typow prostych i wywolanie metody w przypadku tablic
+		if(!checker(f, a) && f.getType().isArray()){				
+			converted += "\"" + f.getName() + "\":[";
+			
+			arrayConvert(f, f.get(a));
+			
+			//usuniêcie ostatniego przecinka:
+			converted = converted.substring(0, converted.length()-2);
+			
+			converted += "], ";
+		}
+	}
+	
 	public static void toJSON(Object a) throws IllegalArgumentException, IllegalAccessException{
 		
 		converted = "{\"" + a.getClass().getName() + "\": {";
 
-		for(Field f: a.getClass().getFields()){
-				
-			//Konwersja dla typow prostych i wywolanie metody w przypadku tablic
-			if(!checker(f, a) && f.getType().isArray()){				
-				converted += "\"" + f.getName() + "\":[";
-				
-				arrayConvert(f, f.get(a));
-				
-				//usuniêcie ostatniego przecinka:
-				converted = converted.substring(0, converted.length()-2);
-				
-				converted += "], ";
-			}
+		for(Field f: a.getClass().getDeclaredFields()){
+				//Sprawdzenie modyfikatora i ew. ustawienie dostepu
+				if(f.getModifiers() == Modifier.PUBLIC){
+					convert(f, a);
+				} else {
+					f.setAccessible(true);
+					convert(f, a);
+				}
 		}
 		
 		//usuniêcie ostatniego przecinka:
